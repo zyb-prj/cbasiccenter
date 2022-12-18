0b3f84555c7dc704c1d5e8bd666246840be6d7bd
zyb <zyb_2457@outlook.com>
2022-12-18 13:03:43 +0800

    重置cbasiccenter工程

--- array/04_字符数组和字符串/计算字符串长度.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int	main(int argc, char **argv)
-{
-    char str[] = "zyb666";
-    int length = strlen(str);
-    printf("The lenth of the string is %ld.\n", length);
-}
--- array/06_字符串处理函数/字符串复制函数.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-int main(){
-    char str1[50] = "hahahaa";
-    char str2[50] = "hehe";
-    strcpy(str1, str2);
-    printf("str1: %s\n", str1);
-    return 0;
-}
